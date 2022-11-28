71ce3bfd22b0ad5fa8e8a65e767ed028c1e3b66c
Philipp Korber <philipp@korber.dev>
2022-11-28 10:58:36 +0100

    Used trick to add Client and Server type.

--- src/client/client.ts
+++ src/client/client.ts
@@ -4,3 +4,6 @@ import { LikeDocumentMixin, PersonalizedDocumentMixin } from "./mixins/mixins";
 export * from "./model/errors";
 export * from "./model/model";
 export const Client = PersonalizedDocumentMixin(LikeDocumentMixin(BaseClient));
+
+const _client = new Client({ token: '', endpoint: 'https://d.invalid', environment: '', userId: '' });
+export type Client = typeof _client;
