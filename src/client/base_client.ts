8e84fdb835d1d5b7d35a8d85c37ac2c3285a3ceb
Philipp Korber <philipp@korber.dev>
2022-12-01 11:23:03 +0100

    remove environment

--- src/client/base_client.ts
+++ src/client/base_client.ts
@@ -4,19 +4,16 @@ export type BaseClientCtr = _Constructor<BaseClient>;
 
 export class BaseClient {
   readonly endpoint: URL;
-  readonly environment: string;
   readonly token: string;
   readonly userId: string;
 
   constructor(args: {
     readonly token: string;
     readonly endpoint: string | URL;
-    readonly environment: string;
     readonly userId: string;
   }) {
     this.token = args.token;
-    this.endpoint = args.endpoint instanceof URL ? args.endpoint : new URL(args.endpoint);
-    this.environment = args.environment;
+    this.endpoint = new URL(args.endpoint);
     this.userId = args.userId;
   }
 }
--- src/server/base_server.ts
+++ src/server/base_server.ts
@@ -4,16 +4,13 @@ export type BaseServerCtr = _Constructor<BaseServer>;
 
 export class BaseServer {
   readonly endpoint: URL;
-  readonly environment: string;
   readonly token: string;
 
   constructor(args: {
     readonly token: string;
-    readonly endpoint: string;
-    readonly environment: string;
+    readonly endpoint: string | URL;
   }) {
     this.token = args.token;
     this.endpoint = new URL(args.endpoint);
-    this.environment = args.environment;
   }
 }
